b110cb2171abfa0b551d6a25102b9921f351e008
Maximilian Weigand <mweigand@mweigand.net>
2023-12-08 13:30:58 +0100

    update pinenote-dbus-service dbus description and prepare to receive performance-mode requests via a debug signal

--- pnhelper@m-weigand.github.com/ebc.js
+++ pnhelper@m-weigand.github.com/ebc.js
@@ -7,7 +7,7 @@ const GLib = imports.gi.GLib;
 // dbus-send --system --print-reply --dest=org.pinenote.ebc /ebc org.freedesktop.DBus.Introspectable.Introspect
 // make sure to fix the <node> tag and remove the "name"
 const PinenoteDbusInterface = `
-<node >
+<node>
   <interface name="org.pinenote.ebc">
     <method name="GetAutorefresh">
       <arg name="state_autorefresh" type="b" direction="out"/>
@@ -15,14 +15,23 @@ const PinenoteDbusInterface = `
     <method name="GetBwMode">
       <arg name="current_mode" type="y" direction="out"/>
     </method>
+    <method name="GetDclkSelect">
+      <arg name="dclk_select" type="y" direction="out"/>
+    </method>
     <method name="GetDefaultWaveform">
       <arg name="current_waveform" type="y" direction="out"/>
+    </method>
+    <method name="RequestQualityOrPerformanceMode">
+      <arg name="mode_request" type="y" direction="in"/>
     </method>
     <method name="SetAutoRefresh">
       <arg name="state" type="b" direction="in"/>
     </method>
     <method name="SetBwMode">
       <arg name="new_mode" type="y" direction="in"/>
+    </method>
+    <method name="SetDclkSelect">
+      <arg name="state" type="y" direction="in"/>
     </method>
     <method name="SetDefaultWaveform">
       <arg name="waveform" type="y" direction="in"/>
@@ -33,9 +42,12 @@ const PinenoteDbusInterface = `
     </method>
     <method name="TriggerGlobalRefresh">
     </method>
-    <signal name="WaveformChanged">
-    </signal>
     <signal name="BwModeChanged">
+    </signal>
+    <signal name="ReqQualityOrPerformance">
+      <arg name="requested_mode" type="y"/>
+    </signal>
+    <signal name="WaveformChanged">
     </signal>
     <property name="default_waveform" type="y" access="readwrite"/>
   </interface>
@@ -61,5 +73,15 @@ function ebc_subscribe_to_waveformchanged(func, widget){
 	PnProxy.connectSignal(
 		"WaveformChanged", func_signal
 	);
+}
 
+// the pinenote-dbus-service can emit a signal which indicates that a
+// performance-mode-change was requested
+function ebc_subscribe_to_requestperformancemode(func, widget){
+	function func_signal (connection, sender, path, iface, signal, params){
+		func(connection, sender, path, iface, signal, params, widget);
+	}
+	PnProxy.connectSignal(
+		"ReqQualityOrPerformance", func_signal
+	);
 }
